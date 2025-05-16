229a2140d98be189ed1e1cf53be70834bbc1d439
OSHIN <oshin@biggestlab.io>
2025-05-16 15:23:02 +0700

    Update todo_server.ex

--- lib/todo_app/todo_server.ex
+++ lib/todo_app/todo_server.ex
@@ -2,15 +2,33 @@ defmodule TodoApp.TodoServer do
   use GenServer
 
   # Client | Start GenServer
-  def start_link(_args) do
-    GenServer.start_link(__MODULE__, :counter, name: __MODULE__)
+  def start_link(initial_value \\ 0)
+    GenServer.start_link(__MODULE__, initial_value, name: __MODULE__)
+  end
+
+  def increment do
+    GenServer.cast(__MODULE__, :increment)
+  end
+
+  def get do
+    GenServer.call(__MODULE__, :get)
   end
 
 
   # Server (Callbacks)
   @impl true
-  def init(:counter) do
+  def init(initial_value) do
     IO.puts("genserver!")
-    {:ok, 0}
+    {:ok, initial_value}
+  end
+
+  @impl true
+  def handle_cast(:increment, count) do
+    {:noreply, count + 1}
+  end
+
+  @impl true
+  def handle_call(:get, _from, count) do
+    {:reply, count, count}
   end
 end
