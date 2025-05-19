45c4a80423bc196a07200395a7532e4e60306451
OSHIN <oshin@biggestlab.io>
2025-05-19 13:40:46 +0700

    Update todo_app_live.ex

--- lib/todo_app_web/live/todo_app_live.ex
+++ lib/todo_app_web/live/todo_app_live.ex
@@ -1,6 +1,34 @@
 defmodule TodoAppWeb.TodoAppLive do
   use TodoAppWeb, :live_view
 
+  def mount(_params, _session, socket) do
+    count = TodoApp.TodoServer.get()
+    IO.puts("count on mount #{inspect(count)}")
+    {:ok, assign(socket, count: count)}
+  end
+
+  def handle_event("increment", _params, socket) do
+    TodoApp.TodoServer.increment()
+    count = TodoApp.TodoServer.get()
+    IO.puts("count on increment #{inspect(count)}")
+    {:noreply, assign(socket, count: count)}
+  end
+
+  def handle_event("decrement", _params, socket) do
+    TodoApp.TodoServer.decrement()
+    count = TodoApp.TodoServer.get()
+    IO.puts("count on decrement #{inspect(count)}")
+    {:noreply, assign(socket, count: count)}
+  end
+
+  def handle_event("reset", _params, socket) do
+    TodoApp.TodoServer.reset()
+    count = TodoApp.TodoServer.get()
+    IO.puts("count on reset #{inspect(count)}")
+    {:noreply, assign(socket, count: count)}
+  end
+
+  @spec render(any()) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
     ~H"""
       <main class="bg-sky-100 flex flex-col gap-y-8 h-screen items-center pt-40">
@@ -10,9 +38,9 @@ defmodule TodoAppWeb.TodoAppLive do
         </header>
         <section class="flex flex-col items-center gap-y-10">
           <div class="flex gap-x-10 text-3xl text-white">
-           <button class="rounded-full w-10 h-10 border border-black shadow-lg bg-slate-900"> - </button>
-            <span class="text-black">0</span>
-           <button class="rounded-full w-10 h-10 border border-black shadow-lg bg-slate-900"> + </button>
+           <button class="rounded-full w-10 h-10 border border-black shadow-lg bg-slate-900" phx-click="decrement"> - </button>
+            <span class="text-black"><%= @count %></span>
+           <button class="rounded-full w-10 h-10 border border-black shadow-lg bg-slate-900" phx-click="increment"> + </button>
           </div>
           <button class="text-lg">Reset</button>
         </section>
